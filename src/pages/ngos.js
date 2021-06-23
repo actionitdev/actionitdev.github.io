b8c492fa85d27ab3b37e0db938e8e2368f09e9ed
Peter.Chen1 <peter.chen@monash.edu>
2021-06-23 23:35:03 +1000

    Add CNAME to Build

--- src/pages/ngos.js
+++ src/pages/ngos.js
@@ -42,12 +42,13 @@ export default function NgoPage() {
               </p>
 
               <p>
-                Action IT had a good initial run for S1 2021. Last burst, we had
-                51 applicants with different skills and technical background. We
-                had 4 successful projects related to different areas, including
-                accessibility, data retrieval, translation, and CI/CD. These
-                projects were done over 720 hours by 9 competent student
-                volunteers who made through an extensive application process.
+                Action IT had a good initial run for S1 2021. For the latest
+                burst, we had 51 applicants with different skills and technical
+                background. We had 4 successful projects related to different
+                areas, including accessibility, data retrieval, translation, and
+                CI/CD. These projects were done over 720 hours by 9 competent
+                student volunteers who made through an extensive application
+                process.
               </p>
             </div>
           </div>
@@ -73,6 +74,33 @@ export default function NgoPage() {
               <p>Development Hours</p>
             </div>
           </div>
+        </div>
+      </section>
+
+      <section id="how-we-work" className="section-bg">
+        <div className="container">
+          <header className="section-header">
+            <h3>How We Work</h3>
+          </header>
+
+          <p>
+            Action IT operates "Burst" mode working to ensure the successful
+            delivery of infrastructures and solutions. Each Development burst
+            lasts for 10 weeks, which allows Action IT volunteers to work
+            intensively on existing projects for NGOs. Projects are signed-off
+            and handed over to NGOs at the end of each Development burst with
+            detailed documentation.
+            <br />
+            <br />
+            Due to the nature of the burst mode, all Action IT projects need to
+            be well-scoped to make sure the volunteers can deliver the projects
+            on time with good qualities. Amid Development bursts, Action IT also
+            runs Design and Scoping bursts where goals are set and prioritized.
+            Major deliverables, project schedules, and milestones are defined
+            and project plans are developed. Please get in touch with us if you
+            would like to know how Action IT can help with your organisation and
+            whether your projects will fit our burst mode.
+          </p>
         </div>
       </section>
     </Layout>
